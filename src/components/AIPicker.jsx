c86e18ba694edf8666bef5dfd654cb274e4f597f
GedasIncirauskas <gedas.inc@gmail.com>
2023-10-24 10:36:37 +0300

    added components

--- src/components/AIPicker.jsx
+++ src/components/AIPicker.jsx
@@ -1,5 +1,45 @@
-const AIPicker = () => {
-  return <div>AIPicker</div>;
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI..."
+        rows={4}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <Button type="outline" title="Asking AI..." customStyles="text-xs" />
+        ) : (
+          <>
+            <Button
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit("logo")}
+              customStyles="text-xs"
+            />
+            <Button
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit("full")}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AIPicker;
+
+AIPicker.propTypes = {
+  prompt: PropTypes.string,
+  setPrompt: PropTypes.func,
+  generatingImg: PropTypes.bool,
+  handleSubmit: PropTypes.func,
+};
--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -1,5 +1,45 @@
-const FilePicker = () => {
-  return <div>FilePicker</div>;
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const FilePicker = ({ file, setFile, readFile }) => {
+  return (
+    <div className="filepicker-container">
+      <div className="flex-1 flex flex-col">
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label htmlFor="file-upload" className="filepicker-label">
+          Upload picture
+        </label>
+
+        <p className="mt-2 ml-1 text-gray-500 text-sm truncate">
+          {file === "" ? "No file selected" : file.name}
+        </p>
+      </div>
+
+      <div className="mt-4 flex flex-wrap gap-2">
+        <Button
+          type="filled"
+          title="Full"
+          handleClick={() => readFile("full")}
+        />
+        <Button
+          type="outline"
+          title="Only logo"
+          handleClick={() => readFile("logo")}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default FilePicker;
+
+FilePicker.propTypes = {
+  file: PropTypes.any,
+  setFile: PropTypes.func,
+  readFile: PropTypes.func,
+};
